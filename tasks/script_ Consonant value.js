47e6f7821c54a6ff61a72b9556fec39b57697a11
Marinade <marinadegames@gmail.com>
2021-11-13 19:07:27 +0300

    ADD TASK | Consonant value

---
+++ tasks/script_ Consonant value.js
@@ -0,0 +1,12 @@
+
+let str = 'ZODIAC'; // 26
+
+function solve(s) {
+    s=s.toLowerCase();
+    let arr = s.slice().replace(/[aeiuo]/g,' ').replace(/\s+/g,' ');
+    arr=arr.split(' ').map(v=>v.split('').map(v=>v.charCodeAt()-96).reduce((a,b)=>a+b,0))
+    return Math.max(...arr);
+};
+
+
+console.log(solve(str));
