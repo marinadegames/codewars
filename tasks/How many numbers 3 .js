5a8caef353195019f317201ce1781bef8cc783ba
Marinade <marinadegames@gmail.com>
2021-11-30 22:02:36 +0300

    fix task | HowManyNumbers | This script working, but he is very slowly... Need fix

--- tasks/How many numbers 3 .js
+++ tasks/How many numbers 3 .js
@@ -1,67 +1,69 @@
-
-
-
 function findAll(n, k) {
-
     let max = Number(maxNumber(k))
     let min = Number(minNumber(k))
     let arr = numberGeneration(min, max)
-    
-		let res = findNumber(arr, n);
-    return res
-    
+    return findNumber(arr, n)
 }
 
-function minNumber (l){
-	let arr = []
-  
-  for (let i = 0; i < l; i++){
-  	if (i == 0) { arr.push(1) }
-    else { arr.push(0) }
-}
-return arr.join('');
-}
-function maxNumber (l){
-	let arr = []
-  
-  for (let i = 0; i < l; i++){
-  	arr.push(9);
-}
-return arr.join('');
-}
-function numberGeneration (min, max){
-	let arr = [];
-  for(let i = min; i < max+1; i++){
-  	arr.push(i)
-  }
-  return arr;
-}
+function minNumber(l) {
+    let arr = []
 
-function findNumber (arr, target){
-	
-  let resArray = []
-  let count = 0
-  
-	for (let i = 0; i < arr.length; i++){
-  	let a = String(arr[i]);
-    let b = a.split('');
-    let sum = 0
-    
-    for (let j = 0; j < b.length; j++){
-      sum = sum + Number(b[j]);
+    for (let i = 0; i < l; i++) {
+        if (i === 0) {
+            arr.push(1)
+        } else {
+            arr.push(0)
+        }
     }
-    
-    if (sum === target){
-    	resArray.push(arr[i]);
-      count = count + 1
-    }
-  }
-  
-  let max = resArray[resArray.length - 1]
-  let min = resArray[0]
-  return [count, min, max]
-  
-  
+    return arr.join('');
 }
 
-console.log(findAll(27, 3)) // [8, '118', '334']);
+function maxNumber(l) {
+    let arr = []
+
+    for (let i = 0; i < l; i++) {
+        arr.push(9);
+    }
+    return arr.join('');
+}
+
+function numberGeneration(min, max) {
+    let arr = [];
+    for (let i = min; i < max + 1; i++) {
+        arr.push(i)
+    }
+    return arr;
+}
+
+function findNumber(arr, target) {
+
+    let numArr = arr.map(value => String(value));
+    let count = 0;
+    let result = [];
+    for (let i = 0; i < numArr.length; i++) {
+
+        let array = numArr[i].split(''); // разделили на массив
+        let sum = 0; // сумма чисел массива
+        let sort = array.slice(0).sort();
+        let res = [];
+
+        for (let j = 0; j < array.length; j++) { // сумма чисел в массиве
+            sum = sum + Number(array[j]);
+        }
+
+        if (sum === target && Number(array.join('')) === Number(sort.join(''))) {
+            count = count + 1
+            //console.log(`Сумма ${array} равна ${target}. Кол-во: ${count}`)
+            result.push(array.join(''));
+        }
+
+    }
+    if (count === 0){
+        return result
+    }
+    //console.log([count, result[0], result[result.length-1]])
+    return [count, result[0], result[result.length-1]]
+}
+
+
+console.log(findAll(10, 3)) // [8, '118', '334']);
