e91e7d922b74b16b34e247431031134816afe1bc
Marinade <marinadegames@gmail.com>
2021-12-11 19:04:14 +0300

    ADD TASK | binaryArrayToNumber

---
+++ tasks/Ones and Zeros.js
@@ -0,0 +1,19 @@
+const binaryArrayToNumber = arr => {
+
+
+    let num = arr.join('')
+    return parseInt( num, 2 )
+};
+
+let arr = [1, 0, 0, 1]
+console.log(binaryArrayToNumber(arr))
+
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
