ed2c2a9cdf4e6a5ee7a8f37c3056fdb8a41aa268
marinadedev <87521761+marinadegames@users.noreply.github.com>
2022-02-03 12:35:22 +0300

    fix solution

--- tasks/Sum Arrays.js
+++ tasks/Sum Arrays.js
@@ -1,7 +1,3 @@
-// Sum Numbers
 function sum (numbers) {
-    "use strict";
-    
-    if(numbers.length !== 0) return numbers.reduce((a,b)=> a+b)
-  else return 0
+    return numbers.reduce( (a,b) => a+b, 0)
 };
